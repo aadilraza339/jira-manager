689949f765b199792a5aed2c266de6075d8cfda6
aadilraza339 <aadil18@navgurukul.org>
2023-04-20 14:04:14 +0530

    update the route and change the path name

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,7 +1,7 @@
 const ticketsController = require("../controllers/tickets")
 const routes = require("express").Router();
 
-routes.use("/tickets", ticketsController.fetchIssues);
+routes.get("/tickets/reload", ticketsController.updateTicketsRecords);
 
 
 module.exports = routes;
