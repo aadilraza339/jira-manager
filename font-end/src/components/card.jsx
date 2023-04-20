7138e9cb5ff6cddade14df7c8c7abe1354079fe8
aadilraza339 <aadil18@navgurukul.org>
2023-04-20 18:39:37 +0530

    Aadil reload button

--- font-end/src/components/card.jsx
+++ font-end/src/components/card.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 function Card() {
     const [tickets, setTickets] = useState([])
+    const [refresh,setRefresh] = useState(false)
     const fetchTickets = async () => {
         try {
             const response = await axios.get('http://localhost:3000/api/v1/tickets')
@@ -15,11 +16,33 @@ function Card() {
     useEffect(() => {
         fetchTickets()
     }, [])
- 
+    const  refreshTickets = async()=>{
+        setRefresh(true)
+        try {
+            const response = await axios.get('http://localhost:3000/api/v1/tickets/reload')
+            setTickets(response.data.data)
+            setRefresh(false)
+        } catch (error) {
+            console.log(error);
+            setRefresh(false)
+        }
+    }
     return (
         <>
             <h1>Jira Manager</h1>
-           
+            <div className="sub-header">
+                <div className="reload-box">
+                    {!refresh ?
+                    
+                (
+                    <span onClick={refreshTickets}>Reload</span>
+
+                )
+                :   <img src="/icons8-refresh.gif" alt="My Image" />
+            }
+
+                </div>
+            </div>
             {!tickets.length ? (
                 <div className="">Loaing</div>
             )
