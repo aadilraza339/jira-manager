292ad9e7d5206de0f07636181799640742a0878e
aadilraza339 <aadil18@navgurukul.org>
2023-04-20 14:07:02 +0530

    Update the logic to handle duplicate records and ensure that they are stored correctly in the database

--- model/query.js
+++ model/query.js
@@ -1,11 +1,21 @@
 
 const knex = require("./connection");
 
-const insertTickets = (tickets)=>{
-    return knex("tickets").insert(tickets) 
+const updateTicketsData = (ticket)=>{
+    return knex.transaction(
+		async (trx) => {
+			const ticketDetails = await trx("tickets").where({"number":ticket.number})
+            if(ticketDetails.length) {
+                await trx("tickets").update(ticket).where({"number":ticket.number})
+            }
+            else {
+               await trx("tickets").insert(ticket) 
+            }
+
+		}
+	)
 }
 
-
 module.exports  = {
-    insertTickets,
+    updateTicketsData,
 }
